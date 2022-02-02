7efaba985fa8bf9020fb47190dc05e4daed5e826
Charles Christensen <charles.n.chr@gmail.com>
2022-02-02 21:59:23 +0000

    Attempts with Q2

--- keyboards/keychron/q2/rev_0111/rules.mk
+++ keyboards/keychron/q2/rev_0111/rules.mk
@@ -5,7 +5,7 @@ MCU = STM32L433
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
