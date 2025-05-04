a869a4906546394b781844473722754aebd303b7
Charles Christensen <charles.n.chr@gmail.com>
2025-05-04 17:57:46 +0100

    Added bindings for powertoys to show danish special chars on windows

--- keyboards/silakka54/keymaps/default/keymap.c
+++ keyboards/silakka54/keymaps/default/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(2, KC_GRV),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         LCTL_T(KC_ESC), LSFT_T(KC_A),    LALT_T(KC_S),    LGUI_T(KC_D),   LCTL_T(KC_F),    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    LT(5, KC_SCLN), KC_QUOT,
-        KC_LSFT, LSFT_T(KC_Z),    KC_X,    KC_C,    LT(5, KC_V),    KC_B,                               RALT_T(KC_N),    LALT_T(KC_M), RGUI_T(KC_COMM),  RCTL_T(KC_DOT),  RSFT_T(KC_SLSH), LCTL_T(KC_RBRC),
+        KC_LSFT, LSFT_T(KC_Z),    LT(7, KC_X),    KC_C,    LT(5, KC_V),    KC_B,                               LT(2, KC_N),    LALT_T(KC_M), RGUI_T(KC_COMM),  RCTL_T(KC_DOT),  RSFT_T(KC_SLSH), LCTL_T(KC_RBRC),
                                            KC_LGUI, LALT_T(KC_MINS), LT(2, KC_SPC),             LGUI_T(KC_SPC), KC_BSPC,  LT(4, KC_ENT)
     ),
     [1] = LAYOUT(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
         KC_ENT,  KC_HOME,    KC_UP,    KC_END,    KC_VOLU, KC_MNXT,                      KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_F11,  KC_F12,
         MO(3),  KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD,  KC_MPRV,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_EQL, KC_PLUS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS,
+        KC_TRNS,  KC_CAPS, KC_TRNS, KC_TRNS, KC_EQL, KC_PLUS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     [3] = LAYOUT(
@@ -59,6 +59,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            qwerty, gamer, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                            KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
+    ),
+    [7] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, RCTL(RALT(RSFT(KC_F1))), RCTL(RALT(RSFT(KC_F2))), RCTL(RALT(RSFT(KC_F3))), KC_TRNS, KC_TRNS,
+        KC_TRNS, MO(8), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                            KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
+    ),
+    [8] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, RCTL(RALT(RSFT(RGUI(KC_F1)))), RCTL(RALT(RSFT(RGUI(KC_F2)))), RCTL(RALT(RSFT(RGUI(KC_F3)))), KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
 };
